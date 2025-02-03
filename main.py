99bf9585a14a6a8de0ca89657b6d79dcd0f48106
Gerald Desmond Readwin <141354136+GDRead3@users.noreply.github.com>
2025-02-03 11:55:16 -0700

    Update count_character function
    added "isalpha" statement to filter out special characters and cleaned up the presentation of the report

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     lowered_text = turn_lowercase(text)
     characters_dict = count_characters(lowered_text)
-    print (characters_dict)
-
+    print ("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    for key in characters_dict:
+        print(f"The '{key}' character was found {characters_dict[key]} times")
+    print ("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -23,10 +25,11 @@ def turn_lowercase(text):
 def count_characters(text):
     characters = {}
     for char in text:
-        if char in characters:
-            characters[char] = characters[char] + 1
-        else:
-            characters[char] = 1
+        if char.isalpha():
+            if char in characters:
+                characters[char] = characters[char] + 1
+            else:
+                characters[char] = 1
     return characters
 
 

3771f21394d8f143a3c3235ccb639ba2e4ce1f41
Gerald Desmond Readwin <141354136+GDRead3@users.noreply.github.com>
2025-02-03 11:22:34 -0700

    Added count character function
    returns a dictionary with each characters as a key and there respective value is the count within the text. requires text as a sngle string input

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    test = text.split()
+    lowered_text = turn_lowercase(text)
+    characters_dict = count_characters(lowered_text)
+    print (characters_dict)
+
 
 def get_num_words(text):
     words = text.split()
@@ -13,6 +16,18 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def turn_lowercase(text):
+    return text.lower()
+
+def count_characters(text):
+    characters = {}
+    for char in text:
+        if char in characters:
+            characters[char] = characters[char] + 1
+        else:
+            characters[char] = 1
+    return characters
 
 
 main()
